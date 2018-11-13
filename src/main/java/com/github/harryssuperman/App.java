30506289531604994ae228bc60e2d0ee4feefc88
Manuel Sierra <harryssuperman@hotmail.com>
2018-11-14 00:58:27 +0100

    Init project

---
+++ src/main/java/com/github/harryssuperman/App.java
@@ -0,0 +1,13 @@
+package com.github.harryssuperman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
